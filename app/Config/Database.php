76268f16e5e61f56aad4e23e5957e1ec331df0c7
Shivam Vishwakarma <shiva83023@gmail.com>
2024-12-13 23:14:30 +0530

    Set up the configuration

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,7 +26,8 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '127.0.0.1',
+        // 'hostname'     => '127.0.0.1',
+        'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
         'database'     => 'task_manager',
